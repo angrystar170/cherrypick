defb8d0946f834cc90da5888ed0fd331c06f2064
angrystar170 <angrystar170@gmail.com>
2025-11-03 18:22:40 +0900

    Update font loading method for ONEMobilePOP
    Replaced font-face declaration with an import statement for ONEMobilePOP.

--- packages/backend/src/server/web/boot.js
+++ packages/backend/src/server/web/boot.js
@@ -232,12 +232,7 @@
 		errorsElement.appendChild(detailsElement);
 		addStyle(`
 		
-		@font-face {
-			font-family: 'ONEMobilePOP';
-			src: url('https://misskey.njy17.kr/files/8219ac10-832c-4a75-b5f6-8703fa0d72f6') format('woff');
-			font-weight: normal;
-			font-style: normal;
-		}
+		@import url("https://page.njy17.kr/ONEMobilePOP.css");
 		
 		* {
 			font-family: 'ONEMobilePOP', BIZ UDGothic, Roboto, HelveticaNeue, Arial, sans-serif;
